2798157ea08d6b5ddf785522447cd52acbaedf50
Kamron Bhavnagri <kamwithk@tuta.io>
2023-11-13 19:16:18 +1100

    Mount media and backup partitions of external harddrive

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,9 +60,19 @@
     LC_TIME = "en_AU.UTF-8";
   };
 
-  # Mount Windows partition
+  # Mount partitions
   fileSystems."/mnt/Windows" = {
     device = "/dev/nvme0n1p4";
+    fsType = "auto";
+    options = [ "nosuid" "nodev" "nofail" "x-gvfs-show"];
+  };
+  fileSystems."/mnt/Media" = {
+    device = "/dev/sda1";
+    fsType = "auto";
+    options = [ "nosuid" "nodev" "nofail" "x-gvfs-show"];
+  };
+  fileSystems."/mnt/Backup" = {
+    device = "/dev/sda2";
     fsType = "auto";
     options = [ "nosuid" "nodev" "nofail" "x-gvfs-show"];
   };
