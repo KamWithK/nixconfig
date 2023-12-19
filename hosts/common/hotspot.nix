a9f8da86c9412b04d0e2e7ecc51d9f187c6ac46d
Kamron Bhavnagri <kamwithk@tuta.io>
2023-12-19 13:07:08 +1100

    Switched hotspot internet interface

--- hosts/common/hotspot.nix
+++ hosts/common/hotspot.nix
@@ -3,7 +3,7 @@
   services.create_ap = {
     enable = true;
     settings = {
-      INTERNET_IFACE = "wg-mullvad";
+      INTERNET_IFACE = "enp42s0";
       WIFI_IFACE = "wlp33s0";
       SSID = "WIFI";
       PASSPHRASE = "";
