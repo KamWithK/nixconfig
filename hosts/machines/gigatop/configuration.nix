2336be138d6ee75a2e364e6035ba67ba7dd20a43
Kamron Bhavnagri <kamwithk@tuta.io>
2023-12-19 13:00:47 +1100

    Use polkit

--- hosts/machines/gigatop/configuration.nix
+++ hosts/machines/gigatop/configuration.nix
@@ -50,6 +50,24 @@
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;
 
+  # Polkit
+  security.polkit.enable = true;
+  systemd = {
+  user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   # Logitech Unifying Bolt
   hardware.logitech.wireless.enable = true;
   hardware.logitech.wireless.enableGraphical = true;
@@ -70,6 +88,7 @@
       home-manager
 
       killall
+      polkit_gnome
       parted
 
       alacritty
