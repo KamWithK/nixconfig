30e1c7b75833ea9df434f8e29d9a9c4dbcc4c18d
Kamron Bhavnagri <kamwithk@tuta.io>
2023-12-19 12:59:36 +1100

    Include Mullvad's exclude wrapper

--- hosts/common/vpn.nix
+++ hosts/common/vpn.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
   services.mullvad-vpn.enable = true;
-  services.mullvad-vpn.enableExcludeWrapper = false;
+  services.mullvad-vpn.enableExcludeWrapper = true;
   services.mullvad-vpn.package = pkgs.unstable.mullvad-vpn;
 }
