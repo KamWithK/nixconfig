d7e2d39a6e842b731c15d0278d042b16e36c6cdc
Kamron Bhavnagri <kamwithk@tuta.io>
2024-10-18 19:41:12 +1100

    Swap to RFC Nix format

--- home-manager/common/doom_emacs.nix
+++ home-manager/common/doom_emacs.nix
@@ -6,7 +6,7 @@
     doomDir = ../../dotfiles/doom;
     emacs = pkgs.emacs29-pgtk;
     extraPackages = epkgs: config.programs.emacs.extraPackages epkgs ++ [ epkgs.treesit-grammars.with-all-grammars ];
-    extraBinPackages = with pkgs; [ git ripgrep fd ispell shellcheck shfmt pandoc nixfmt-classic nodePackages.npm ];
+    extraBinPackages = with pkgs; [ git ripgrep fd ispell shellcheck shfmt pandoc nixfmt-rfc-style nodePackages.npm ];
   };
   services.emacs.enable = true;
 }
