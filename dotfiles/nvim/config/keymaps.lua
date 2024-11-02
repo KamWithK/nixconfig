249fc8b6103c60cc214c6cd38e6bd404669bed5b
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-03 10:32:52 +1100

    Neovim keybinds to insert new lines above/below the current line, like in Helix

--- dotfiles/nvim/config/keymaps.lua
+++ dotfiles/nvim/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "[<space>", "m`O<Esc>0D``", { desc = "Insert Line Above" })
+vim.keymap.set("n", "]<space>", "m`o<Esc>0D``", { desc = "Insert Line Below" })
