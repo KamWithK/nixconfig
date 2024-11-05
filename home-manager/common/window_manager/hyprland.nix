06a195c8bd6e6db6d6ba12f7f4f54153c2c876f1
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-05 11:21:10 +1100

    Hyprland keybinds to navigate to workspaces 6-10

--- home-manager/common/window_manager/hyprland.nix
+++ home-manager/common/window_manager/hyprland.nix
@@ -52,6 +52,11 @@
         "$mod, 3, workspace, 3"
         "$mod, 4, workspace, 4"
         "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
 
         "$mod SHIFT, 1, movetoworkspace, 1"
         "$mod SHIFT, 2, movetoworkspace, 2"
