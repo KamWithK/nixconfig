76d80aca036ccae4930c3cb14134c5b1d84fca01
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-06 18:22:44 +1100

    Don't switch Bluetooth headphones to headset mode

--- hosts/common/pipewire.nix
+++ hosts/common/pipewire.nix
@@ -10,6 +10,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    # Don't switch to headset
+    wireplumber.extraConfig."11-bluetooth-policy" = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+    };
+
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
 
