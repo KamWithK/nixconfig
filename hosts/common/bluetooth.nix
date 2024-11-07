f7ec83a44674dc6b96ddfb7f5ffc67519c2b524a
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-07 18:09:53 +1100

    Fix for bluetooth headphones not connecting without repairing

--- hosts/common/bluetooth.nix
+++ hosts/common/bluetooth.nix
@@ -1,6 +1,19 @@
 { ... }:
 {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+
+    settings = {
+      General = {
+        ControllerMode = "dual";
+        # A2DP support
+        Enable = "Source,Sink,Media,Socket";
+        # Battery level display
+        Experimental = true;
+      };
+    };
+  };
+
   services.blueman.enable = true;
 }
