d1a1b65c169ff4fb513e8d4ca4f09a7878e06005
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-13 23:03:35 +1100

    Yazi shell wrapper

--- home-manager/common/terminal/nushell.nix
+++ home-manager/common/terminal/nushell.nix
@@ -7,6 +7,16 @@
     extraConfig = ''
       $env.config.show_banner = false
       pokeget random --hide-name
+
+      def --env y [...args] {
+        let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+        yazi ...$args --cwd-file $tmp
+        let cwd = (open $tmp)
+        if $cwd != "" and $cwd != $env.PWD {
+          cd $cwd
+        }
+        rm -fp $tmp
+      }
     '';
 
     environmentVariables = {
