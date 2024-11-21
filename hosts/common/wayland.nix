969b98626dc4339093c2cd8aa18f13fe57fd1f58
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-22 08:19:01 +1100

    Remove portal should already be enabled when Hyprland is

--- hosts/common/wayland.nix
+++ hosts/common/wayland.nix
@@ -1,11 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.hyprland = {
     enable = false;
     xwayland.enable = false;
-  };
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
   };
 }
