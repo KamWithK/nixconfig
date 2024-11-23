170d6cf5ea28d592a68119f2c324ab4af80d7ea8
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-23 22:11:29 +1100

    River window rules

--- home-manager/common/window_manager/river.nix
+++ home-manager/common/window_manager/river.nix
@@ -14,6 +14,19 @@
     settings =
       with config.lib.stylix.colors;
       with config.stylix;
+      with lib;
+      let
+        numTags = 5;
+        listToAttrSet = list: listToAttrs (imap (i: nameValuePair (toString i)) list);
+        tagMap = foldl' (x: y: x ++ [ (lib.last x * 2) ]) [ 1 ] (lib.genList (_: [ 1 ]) (numTags - 1));
+        tagMapSet = listToAttrSet (map toString tagMap);
+        tagKeys = concatMapAttrs (index: tag: {
+          "Super ${index}" = "set-focused-tags ${tag}";
+          "Super+Shift ${index}" = "set-view-tags ${tag}";
+          "Super+Control ${index}" = "toggle-focused-tags ${tag}";
+          "Super+Shift+Control ${index}" = "set-view-tags ${tag}";
+        }) tagMapSet;
+      in
       {
         default-layout = "bsp-layout";
         spawn = [ "'river-bsp-layout --inner-gap 3 --outer-gap 10 --split-perc 0.5'" ];
@@ -32,46 +45,48 @@
         ];
 
         map = {
-          normal =
-            with lib;
-            let
-              numTags = 5;
-              listToAttrSet = list: listToAttrs (imap (i: nameValuePair (toString i)) list);
-              tagMap = foldl' (x: y: x ++ [ (lib.last x * 2) ]) [ 1 ] (lib.genList (_: [ 1 ]) (numTags - 1));
-              tagKeys = concatMapAttrs (index: tag: {
-                "Super ${index}" = "set-focused-tags ${toString tag}";
-                "Super+Shift ${index}" = "set-view-tags ${toString tag}";
-                "Super+Control ${index}" = "toggle-focused-tags ${toString tag}";
-                "Super+Shift+Control ${index}" = "set-view-tags ${toString tag}";
-              }) (listToAttrSet tagMap);
-            in
-            tagKeys
-            // {
-              "Super Q" = "spawn foot";
-              "Super W" = "spawn firefox";
-              "Super S" = "spawn \"rofi -show drun -show-icons -display-drun ''\"";
-              "Super+Shift S" = "spawn 'grim -g \"$(slurp)\" - | wl-copy'";
+          normal = tagKeys // {
+            "Super Q" = "spawn foot";
+            "Super W" = "spawn firefox";
+            "Super S" = "spawn \"rofi -show drun -show-icons -display-drun ''\"";
+            "Super+Shift S" = "spawn 'grim -g \"$(slurp)\" - | wl-copy'";
 
-              "Super C" = "close";
-              "Super F" = "toggle-fullscreen";
-              "Super V" = "toggle-float";
+            "Super C" = "close";
+            "Super F" = "toggle-fullscreen";
+            "Super V" = "toggle-float";
 
-              "Super H" = "focus-view left";
-              "Super J" = "focus-view down";
-              "Super K" = "focus-view up";
-              "Super L" = "focus-view right";
-              "Super+Shift H" = "swap left";
-              "Super+Shift J" = "swap down";
-              "Super+Shift K" = "swap up";
-              "Super+Shift L" = "swap right";
+            "Super H" = "focus-view left";
+            "Super J" = "focus-view down";
+            "Super K" = "focus-view up";
+            "Super L" = "focus-view right";
+            "Super+Shift H" = "swap left";
+            "Super+Shift J" = "swap down";
+            "Super+Shift K" = "swap up";
+            "Super+Shift L" = "swap right";
 
-              "None XF86AudioLowerVolume" = "spawn 'volumectl -u down'";
-              "None XF86AudioRaiseVolume" = "spawn 'volumectl -u up'";
-              "None XF86AudioMute" = "spawn 'volumectl toggle-mute'";
-              "None XF86AudioPlay" = "spawn 'playerctl play-pause'";
-              "None XF86AudioNext" = "spawn 'playerctl next'";
-              "None XF86AudioPrev" = "spawn 'playerctl previous'";
-            };
+            "None XF86AudioLowerVolume" = "spawn 'volumectl -u down'";
+            "None XF86AudioRaiseVolume" = "spawn 'volumectl -u up'";
+            "None XF86AudioMute" = "spawn 'volumectl toggle-mute'";
+            "None XF86AudioPlay" = "spawn 'playerctl play-pause'";
+            "None XF86AudioNext" = "spawn 'playerctl next'";
+            "None XF86AudioPrev" = "spawn 'playerctl previous'";
+          };
+        };
+
+        rule-add = {
+          "-app-id" = {
+            "'firefox'" = "ssd";
+            "'thunar'" = "ssd";
+            "'steam'" = "ssd";
+            "'emacs'" = "ssd";
+            "'org.pwmt.zathura'" = "ssd";
+            "'org.pulseaudio.pavucontrol'" = "ssd";
+            "'.blueman-manager-wrapped'" = "ssd";
+            "'org.gnome.Boxes'" = "ssd";
+            "'org.nicotine_plus.Nicotine'" = "ssd";
+            "'discord'" = "tags '${tagMapSet."5"}'";
+            "'Spotify'" = "tags '${tagMapSet."5"}'";
+          };
         };
 
         set-repeat = "50 300";
