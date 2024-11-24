9786c4a81cb0188af01111df5a82a5528c910101
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-24 20:38:06 +1100

    Zellij theme changes upstreamed

--- home-manager/common/terminal/zellij.nix
+++ home-manager/common/terminal/zellij.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.zellij = {
     enable = true;
@@ -9,24 +9,6 @@
   xdg.configFile."zellij/config.kdl" = {
     source = ../../../dotfiles/zellij.kdl;
   };
-
-  xdg.configFile."zellij/themes/stylix.kdl".text = with config.lib.stylix.colors.withHashtag; ''
-    themes {
-      default {
-        bg "${base03}";
-        fg "${base05}";
-        red "${base01}";
-        green "${base0B}";
-        blue "${base0D}";
-        yellow "${base0A}";
-        magenta "${base0E}";
-        orange "${base09}";
-        cyan "${base0C}";
-        black "${base00}";
-        white "${base07}";
-      }
-    }
-  '';
 
   home.shellAliases = {
     zd = "zellij --layout zellij.kdl";
