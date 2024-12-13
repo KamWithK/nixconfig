d87b8bd0bfd4b02e928e9ac8dd85a7470c65cd53
Kamron Bhavnagri <kamwithk@tuta.io>
2024-12-14 09:57:30 +1100

    Remove extra Bluetooth options

--- hosts/common/bluetooth.nix
+++ hosts/common/bluetooth.nix
@@ -3,16 +3,6 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-
-    settings = {
-      General = {
-        ControllerMode = "dual";
-        # A2DP support
-        Enable = "Source,Sink,Media,Socket";
-        # Battery level display
-        Experimental = true;
-      };
-    };
   };
 
   services.blueman.enable = true;
