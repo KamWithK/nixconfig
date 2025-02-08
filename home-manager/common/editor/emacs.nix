28623aeee18dc52fd3bfd5886f9a7c15be261652
Kamron Bhavnagri <kamwithk@tuta.io>
2025-02-08 11:25:10 +1100

    Disable emacs

--- home-manager/common/editor/emacs.nix
+++ home-manager/common/editor/emacs.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.emacs = {
-    enable = true;
+    enable = false;
     package = pkgs.emacs29-pgtk.pkgs.withPackages (epkgs: [
       epkgs.treesit-grammars.with-all-grammars
       epkgs.all-the-icons
