fb0253abeba5a723e7f2adda64c830c4f4293158
Kamron Bhavnagri <kamwithk@tuta.io>
2025-04-12 17:29:52 +1000

    Sunshine game streaming

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -7,6 +7,12 @@
     dedicatedServer.openFirewall = true;
   };
 
+  services.sunshine = {
+    enable = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
 }
