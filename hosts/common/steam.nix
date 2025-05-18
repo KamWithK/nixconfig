7834a468a0072f186b1429fb63a47670d868b361
Kamron Bhavnagri <kamwithk@tuta.io>
2025-05-18 12:32:30 +1000

    Allow Steam network transfers

--- hosts/common/steam.nix
+++ hosts/common/steam.nix
@@ -5,6 +5,7 @@
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
 
   services.sunshine = {
